22a2ab87b15ad26b9b4e63feb1309bb7ddcd4593
Dmitiy87 <daga005999@yandex.ru>
2025-02-13 20:20:22 +0300

    Update urok11.js

---
+++ urok11.js
@@ -0,0 +1,19 @@
+window.addEventListener(
+    'load', 
+    maik_function
+);
+
+function najal(kuda) {
+    console.log(|"Я работаю! нажал (куда)");
+    console.log(kuda);
+}
+
+function main_function() {
+    console.log('Страница готова');
+    kvadrat.addEventListener(
+        'click',
+        (event) => {
+            najal(kvadrat);
+        }
+    )
+}
